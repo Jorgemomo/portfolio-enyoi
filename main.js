41e768d26f11da8b142688b72d192139224bb8e6
Jorgemomo <jmomo581@gmail.com>
2023-10-14 09:59:28 -0500

    Proyecto de portafolio empleando html,css,js-ENYOI

---
+++ main.js
@@ -0,0 +1,47 @@
+const openModal = document.querySelector(".btn--about");
+const closeModalBtn = document.querySelector(".closeModal");
+const modal = document.querySelector("#modal");
+function modalAbout() {
+  modal.showModal();
+}
+
+function closeModal() {
+  modal.close();
+}
+
+openModal.addEventListener("click", modalAbout);
+closeModalBtn.addEventListener("click", closeModal);
+
+// Email.send({
+// Host : "smtp.elasticemail.com",
+// Username : "jmomo581@gmail.com",
+// Password : "F0F4E62FF63C1A6E13241FFFB0582C99403A",
+// To : 'jmomo581@gmail.com',
+// From : "jmomo581@gmail.com",
+// Subject : "Hello desde portfolio",
+// Body : "¡Lo logramos!"
+// }).then(
+// message => alert(message)
+// );
+
+const openMenu = document.querySelector(".btn--menu");
+const closeMenu = document.querySelector(".btn--close");
+const containerMenu = document.querySelector(".menu--container");
+const menuItems = document.querySelectorAll(".menu-item");
+
+function openMenuMobile() {
+  containerMenu.classList.toggle("active");
+}
+
+function closeMenuMobile() {
+  containerMenu.classList.toggle("active");
+}
+
+openMenu.addEventListener("click", openMenuMobile);
+closeMenu.addEventListener("click", closeMenuMobile);
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    containerMenu.classList.toggle("active");
+  });
+});
